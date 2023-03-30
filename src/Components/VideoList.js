e05eb3e1daa441378ed9500b65cc75fd6ce69531
Hirendra77 <119104906+Hirendra77@users.noreply.github.com>
2023-03-30 12:52:32 +0530

    updated

---
+++ src/Components/VideoList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ const VideoList = ({videos, selectedVideo}) => {
+    const renderedList= videos.map((video)=>{
+        return(
+            <div className='video-item' key={video.id.videoId} onClick={()=>selectedVideo(video)}>
+                <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+                <div>{video.snippet.title}</div>
+            </div>
+        )
+    })
+  return (
+   <div className='video-list'>
+{renderedList}
+   </div>
+  )
+}
+export default VideoList
